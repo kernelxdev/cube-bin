8507bcb969216fa43de3e29449b7213ec70d9a85
Kernel <124568923+kernelxdev@users.noreply.github.com>
2024-10-29 19:19:19 +0100

    Initial Commit

---
+++ cube.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define WIDTH 80
+#define HEIGHT 22
+
+void clearScreen() {
+    printf("\033[H\033[J");
+}
+
+void renderCube(float angleX, float angleY, float angleZ) {
+    char buffer[WIDTH * HEIGHT];
+    for (int i = 0; i < WIDTH * HEIGHT; i++) buffer[i] = ' ';
+
+    float cubePoints[8][3] = {
+        {-1, -1, -1}, {1, -1, -1}, {1, 1, -1}, {-1, 1, -1},
+        {-1, -1, 1}, {1, -1, 1}, {1, 1, 1}, {-1, 1, 1}
+    };
+
+    int edges[12][2] = {
+        {0, 1}, {1, 2}, {2, 3}, {3, 0},
+        {4, 5}, {5, 6}, {6, 7}, {7, 4},
+        {0, 4}, {1, 5}, {2, 6}, {3, 7}
+    };
+
+    float cosX = cos(angleX), sinX = sin(angleX);
+    float cosY = cos(angleY), sinY = sin(angleY);
+    float cosZ = cos(angleZ), sinZ = sin(angleZ);
+    float projectedPoints[8][2];
+
+    for (int i = 0; i < 8; i++) {
+        float x = cubePoints[i][0];
+        float y = cubePoints[i][1];
+        float z = cubePoints[i][2];
+
+        float nx = cosY * x + sinY * z;
+        float nz = -sinY * x + cosY * z;
+        float ny = cosX * y - sinX * nz;
+        nz = sinX * y + cosX * nz;
+        x = cosZ * nx - sinZ * ny;
+        y = sinZ * nx + cosZ * ny;
+
+        projectedPoints[i][0] = WIDTH / 2 + x * WIDTH / 4;
+        projectedPoints[i][1] = HEIGHT / 2 - y * HEIGHT / 4;
+
+        int screenX = (int)projectedPoints[i][0];
+        int screenY = (int)projectedPoints[i][1];
+
+        if (screenX >= 0 && screenX < WIDTH && screenY >= 0 && screenY < HEIGHT) {
+            buffer[screenY * WIDTH + screenX] = 'X';
+        }
+    }
+
+    for (int i = 0; i < 12; i++) {
+        int p1 = edges[i][0];
+        int p2 = edges[i][1];
+
+        int x1 = (int)projectedPoints[p1][0];
+        int y1 = (int)projectedPoints[p1][1];
+        int x2 = (int)projectedPoints[p2][0];
+        int y2 = (int)projectedPoints[p2][1];
+
+        int dx = abs(x2 - x1), dy = abs(y2 - y1);
+        int sx = x1 < x2 ? 1 : -1;
+        int sy = y1 < y2 ? 1 : -1;
+        int err = dx - dy;
+
+        while (1) {
+            if (x1 >= 0 && x1 < WIDTH && y1 >= 0 && y1 < HEIGHT) {
+                buffer[y1 * WIDTH + x1] = 'X';
+            }
+            if (x1 == x2 && y1 == y2) break;
+            int e2 = 2 * err;
+            if (e2 > -dy) { err -= dy; x1 += sx; }
+            if (e2 < dx) { err += dx; y1 += sy; }
+        }
+    }
+
+    clearScreen();
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            putchar(buffer[i * WIDTH + j]);
+        }
+        putchar('\n');
+    }
+}
+
+int main() {
+    float angleX = 0.0, angleY = 0.0, angleZ = 0.0;
+    while (1) {
+        renderCube(angleX, angleY, angleZ);
+        angleX += 0.010;
+        angleY += 0.06;
+        angleZ += 0.08;
+        usleep(25000);
+    }
+    return 0;
+}
---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo mv cube /usr/local/bin
+echo "You can now use the command cube in terminal to open the app"
+echo "Press any key to continue..."
+read -n 1 -s
